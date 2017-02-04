a9b9901475cb0ed324f8b662eba92e152ddacf3e
xeeshan <xeeshan@gmail.com>
2017-02-03 19:44:53 -0800

    complete design and result show

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 
 var questions = [
-    ['name of country' ,'pakistan' ,'multan','england','A'],
+    ["React.DOM.h1(  {id: 'my-heading'}, React.DOM.span(null, React.DOM.em(null, 'Hell'), 'o' ),' world!'), Which is Equal??",'pakistan' ,'multan','england','A'],
     ['sadsa','sdas','fasf','asf','B'],
     ['name of country' ,'pakistan' ,'multan','england','A'],
     ['sadsa','sdas','fasf','asf','B']
 
 ] ;
 
-var pos = 0, correct = 0 , choice;
+var pos = 0, correct = 0 , choice ,percentage;
 
 
 
@@ -26,19 +26,18 @@ var quizRender = React.createClass({
      },
   
      _checkAnswer: function(e){
-          choices = document.getElementsByName("choices");
-          
-            for (var i = 0; i < choices.length; i++) {
-                if(choices[i].checked){
-                    choice = choices[i].value;
-                }
-            }
-            
+          choices = document.getElementsByName('answer');
+          console.log(choices);
+          for (var i = 0; i < choices.length; i++) {
+          if(choices[i].checked){
+              choice = choices[i].value;
+          }
+       }
             if(choice == this.props.data[pos][4]){
                 correct++;
                 
             }
-          pos++;
+          
           this.setState({
              quesion: this.props.data[pos][0],
              opt1: this.props.data[pos][1],
@@ -46,45 +45,66 @@ var quizRender = React.createClass({
              opt3: this.props.data[pos][3],
              ans: this.props.data[pos][4],
           })
+          pos++;
 
      },
      _showQuestion: function(){
-          
-            if(pos >= 3){
-                return React.DOM.div(
-                     null,
-                     React.DOM.h1(null,'complete')
+           console.log(pos);
+            if(pos >= 4){
+               percentage = correct*20;
+              return React.DOM.div(
+                    {
+                        id:'complete'
+                    },
+                    React.DOM.span({className:'resultBox'},
+                      React.DOM.h2(null, "PERCENTAGE"),
+                      React.DOM.h2(null, percentage + ' %')
+                      ),
+                     React.DOM.span({className:'resultBox'},
+                      React.DOM.h2(null, "CORRECT ANSWER"),
+                      React.DOM.h2(null, correct)
+                      )
                  )
              } else {
                 
                  return  React.DOM.div(
                          null,
                           React.DOM.div(
-                             null,
-                            React.DOM.h1(null,"QUESTION "+ (pos+1) +" OF 4")
+                             {
+                                 id:'status'
+                             },
+                            React.DOM.h1(null,"QUESTION "+ (pos+1) +" OF " +this.props.data.length )
                           ),
-                         React.DOM.h1(null, this.state.quesion),
+                         React.DOM.h4(
+                             {
+                                 id:'question'
+                             }, (pos+1) +'.  '+this.state.quesion),
                          React.DOM.div(
                              null,
                              React.DOM.input(
                                  {
                                     type: "radio",
+                                    className:'optionBtn',
                                     name: 'answer',
                                     value:'A'
                                  }
                              ),
                               this.state.opt1,
+                              React.DOM.br(null),
                               React.DOM.input(
                                  {
                                     type: "radio",
+                                    className:'optionBtn',
                                     name: 'answer',
                                     value:'B'                          
                                  }
                              ),
                              this.state.opt2,
+                             React.DOM.br(null),
                               React.DOM.input(
                                  {
                                     type: "radio",
+                                    className:'optionBtn',
                                     name: 'answer',
                                     value:'C'
                                  }
@@ -93,6 +113,7 @@ var quizRender = React.createClass({
                          ),
                          React.DOM.button(
                                  {
+                                    id:'nextBtn', 
                                     type: "button",
                                     onClick: this._checkAnswer
                                  },
@@ -106,12 +127,19 @@ var quizRender = React.createClass({
      render: function(){
          return React.DOM.div(
              null,
-             React.DOM.div(null, "QUIZ GAME OF REACT"),
+             React.DOM.div( 
+                    {
+                    id: 'header'
+                    },
+                    React.DOM.h1(null,"QUIZ GAME OF REACT")
+             ),
              React.DOM.div(
                  null,
                 
                  React.DOM.div(
-                     null,
+                     {
+                        id:'test'
+                     },
                      this._showQuestion() 
                   )  
              )
